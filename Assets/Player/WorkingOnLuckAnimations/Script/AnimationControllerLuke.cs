d1864cbe9998e2dfe9a79d19456690f090250198
sroberto27 <robertoenrique2710@hotmail.com>
2022-03-29 14:18:36 -0500

    Adding Crouched(Block) Walk, and Dash.

--- Assets/Player/WorkingOnLuckAnimations/Script/AnimationControllerLuke.cs
+++ Assets/Player/WorkingOnLuckAnimations/Script/AnimationControllerLuke.cs
@@ -32,20 +32,94 @@ public class AnimationControllerLuke : MonoBehaviour
         else
         if (Input.GetButton("Fire2"))
         {
-            Debug.Log("Blockkkk");
-            LuckAnimatior.SetBool("Run", false);
-            LuckAnimatior.SetBool("Jump", false);
-            LuckAnimatior.SetBool("DashFront", false);
-            LuckAnimatior.SetBool("DashLeft", false);
-            LuckAnimatior.SetBool("DashRight", false);
-            LuckAnimatior.SetBool("Slash", false);
-            LuckAnimatior.SetBool("Force", false);
-            LuckAnimatior.SetBool("Block", true);
-            LuckAnimatior.SetBool("Idel", false);
-            LuckAnimatior.SetBool("RunJump", false);
-            LuckAnimatior.SetBool("RunForce", false);
+            if (Input.GetKey(KeyCode.A)|| Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.W))
+            {
+                if (Input.GetKeyDown(KeyCode.LeftShift) && Input.GetKey(KeyCode.W))
+                {
+                    Debug.Log("Blockkkk walking");
+                    LuckAnimatior.SetBool("Run", false);
+                    LuckAnimatior.SetBool("Jump", false);
+                    LuckAnimatior.SetBool("DashFront", true);
+                    LuckAnimatior.SetBool("DashLeft", false);
+                    LuckAnimatior.SetBool("DashRight", false);
+                    LuckAnimatior.SetBool("Slash", false);
+                    LuckAnimatior.SetBool("Force", false);
+                    LuckAnimatior.SetBool("Block", true);
+                    LuckAnimatior.SetBool("Idel", false);
+                    LuckAnimatior.SetBool("RunJump", false);
+                    LuckAnimatior.SetBool("RunForce", false);
+                    LuckAnimatior.SetBool("RunCrouched", true);
+                }
+                else
+                if (Input.GetKeyDown(KeyCode.LeftShift) && Input.GetKey(KeyCode.A))
+                {
+                    Debug.Log("Blockkkk walking");
+                    LuckAnimatior.SetBool("Run", false);
+                    LuckAnimatior.SetBool("Jump", false);
+                    LuckAnimatior.SetBool("DashFront", false);
+                    LuckAnimatior.SetBool("DashLeft", true);
+                    LuckAnimatior.SetBool("DashRight", false);
+                    LuckAnimatior.SetBool("Slash", false);
+                    LuckAnimatior.SetBool("Force", false);
+                    LuckAnimatior.SetBool("Block", true);
+                    LuckAnimatior.SetBool("Idel", false);
+                    LuckAnimatior.SetBool("RunJump", false);
+                    LuckAnimatior.SetBool("RunForce", false);
+                    LuckAnimatior.SetBool("RunCrouched", true);
+                }
+                else
+                if (Input.GetKeyDown(KeyCode.LeftShift) && Input.GetKey(KeyCode.D))
+                {
+                    Debug.Log("Blockkkk walking");
+                    LuckAnimatior.SetBool("Run", false);
+                    LuckAnimatior.SetBool("Jump", false);
+                    LuckAnimatior.SetBool("DashFront", false);
+                    LuckAnimatior.SetBool("DashLeft", false);
+                    LuckAnimatior.SetBool("DashRight", true);
+                    LuckAnimatior.SetBool("Slash", false);
+                    LuckAnimatior.SetBool("Force", false);
+                    LuckAnimatior.SetBool("Block", true);
+                    LuckAnimatior.SetBool("Idel", false);
+                    LuckAnimatior.SetBool("RunJump", false);
+                    LuckAnimatior.SetBool("RunForce", false);
+                    LuckAnimatior.SetBool("RunCrouched", true);
+                }
+                else
+                {
+                    Debug.Log("Blockkkk walking");
+                    LuckAnimatior.SetBool("Run", false);
+                    LuckAnimatior.SetBool("Jump", false);
+                    LuckAnimatior.SetBool("DashFront", false);
+                    LuckAnimatior.SetBool("DashLeft", false);
+                    LuckAnimatior.SetBool("DashRight", false);
+                    LuckAnimatior.SetBool("Slash", false);
+                    LuckAnimatior.SetBool("Force", false);
+                    LuckAnimatior.SetBool("Block", true);
+                    LuckAnimatior.SetBool("Idel", false);
+                    LuckAnimatior.SetBool("RunJump", false);
+                    LuckAnimatior.SetBool("RunForce", false);
+                    LuckAnimatior.SetBool("RunCrouched", true);
+                }
+
+            }
+            else {
+                Debug.Log("Blockkkk");
+                LuckAnimatior.SetBool("Run", false);
+                LuckAnimatior.SetBool("Jump", false);
+                LuckAnimatior.SetBool("DashFront", false);
+                LuckAnimatior.SetBool("DashLeft", false);
+                LuckAnimatior.SetBool("DashRight", false);
+                LuckAnimatior.SetBool("Slash", false);
+                LuckAnimatior.SetBool("Force", false);
+                LuckAnimatior.SetBool("Block", true);
+                LuckAnimatior.SetBool("Idel", false);
+                LuckAnimatior.SetBool("RunJump", false);
+                LuckAnimatior.SetBool("RunForce", false);
+                LuckAnimatior.SetBool("RunCrouched", false);
+            }
+                
         }
-        else
+        else   //&& !(Input.GetButtonDown("Fire1") || Input.GetButtonDown("Fire2"))
         if (Input.GetKey("w"))
         {
             if (Input.GetKeyDown(KeyCode.Space))
