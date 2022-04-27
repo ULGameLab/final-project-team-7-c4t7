e2db745062381d7a88a35a4cf9bfe9767bd1fd84
Creese54 <c00408978@louisiana.edu>
2022-04-27 12:45:00 -0500

    Targetable Script Written
    Typed up Targetable script as an interface.

--- Assets/LockOn Player/Targetable.cs
+++ Assets/LockOn Player/Targetable.cs
@@ -2,8 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Targetable : MonoBehaviour
+//public class Targetable : MonoBehaviour
+public interface Targetable
 {
+    //Determines if object is targetable.
+    public bool Targetable { get; }
+    //What the camera will be tracking when object is targeted
+    public Transform TargetTransform { get; }
+
+    /*
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +22,5 @@ public class Targetable : MonoBehaviour
     {
         
     }
+    */
 }
